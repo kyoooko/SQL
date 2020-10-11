4acb5f0ed95543cb992ceaae5f2df07d43d8f650
飯田恭子 <iidakyouko@mba.local>
2020-10-11 18:32:33 +0900

    ドリルA Level7(8章)

--- practice_A_level7.sql
+++ practice_A_level7.sql
@@ -75,3 +75,60 @@ SELECT T.口座番号,
  WHERE T.日付 = '2016-03-01'
 
 --  UNIONだと2016-03-01の取引に限定する方法がない
+
+-- ◆69
+  SELECT T.取引番号, CAST(T.取引事由ID AS VARCHAR) || ':' || J.取引事由名 AS 取引事由, T.日付,T.口座番号,T.入金額,T.出金額
+  FROM 取引 AS T
+  RIGHT JOIN 取引事由 AS J
+    ON T.取引事由ID = J.取引事由ID
+
+--  ◆解答
+-- CAST(J.取引事由ID AS VARCHAR)のとこが多少異なるが正解
+SELECT T.取引番号,
+       CAST(J.取引事由ID AS VARCHAR) || ':' || J.取引事由名 AS 取引事由,
+       T.日付, T.口座番号, T.入金額, T.出金額
+  FROM 取引 AS T
+ RIGHT JOIN 取引事由 AS J
+         ON T.取引事由ID = J.取引事由ID
+
+
+
+  -- ◆70
+  SELECT *
+  FROM 取引事由 AS J
+  RIGHT JOIN 取引 AS T
+    ON J.取引事由ID = T.取引事由ID
+
+--  ◆解答
+SELECT DISTINCT T.取引事由ID, J.取引事由名
+  FROM 取引 AS T
+  FULL JOIN 取引事由 J
+         ON T.取引事由ID = J.取引事由ID
+/*
+-- FULL JOINが使えない場合、以下で代替
+SELECT DISTINCT T.取引事由ID, J.取引事由名
+  FROM 取引 AS T
+  LEFT JOIN 取引事由 J
+         ON T.取引事由ID = J.取引事由ID
+UNION
+SELECT DISTINCT J.取引事由ID, J.取引事由名
+  FROM 取引 AS T
+ RIGHT JOIN 取引事由 J
+         ON T.取引事由ID = J.取引事由ID
+*/
+-- * ❓：主テーブルは取引
+-- * SELECT DISTINCT。SELECT *にしてしまうと両テーブルの列が表示される
+
+-- ◆71
+SELECT K.口座番号, K.名義, K.残高,
+       T.日付,J.取引事由名, T.入金額, T.出金額
+  FROM 口座 AS K
+  JOIN 取引 AS T
+    ON K.口座番号 = T.口座番号
+  JOIN 取引事由 AS J
+    ON T.取引事由ID = J.取引事由ID
+ WHERE K.口座番号 = '0887132'
+ ORDER BY T.取引番号
+
+--  ◆解答
+-- 正解
