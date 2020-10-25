13a2e205d36646db6a4302bb36bc0e77518862e6
Kyooko <12ec153m@al.rikkyo.ac.jp>
2020-10-25 18:25:35 +0900

    ドリルC Leve7(8章)

--- practice_C_level7.sql
+++ practice_C_level7.sql
@@ -48,7 +48,8 @@ SELECT P.ID AS ID, P.名称 AS 名前, CS.コード名称 AS 職業, CJ.コー�
   -- できず→JOIN (SELECT にコード値必要だった
 
   -- 副問合せ
-  SELECT P.ID AS ID, P.名称 AS 名前, (SELECT コード名称 FROM コード AS C WHERE  C.コード種別 = 1) AS 職業, (SELECT コード名称 FROM コード AS C WHERE  C.コード種別 = 2) AS 状態
+  SELECT P.ID AS ID, P.名称 AS 名前, 
+  (SELECT コード名称 FROM コード AS C WHERE  C.コード種別 = 1) AS 職業, (SELECT コード名称 FROM コード AS C WHERE  C.コード種別 = 2) AS 状態
   FROM パーティー AS P
   -- できず
   -- ERROR: more than one row returned by a subquery used as an expression
@@ -70,5 +71,67 @@ SELECT P.ID, P.名称 AS なまえ,
 -- JOIN (SELECT コード値, コード名称〜)にコード値ないとONで繋げないので必要 P.262o
 
 -- ◆66
+SELECT P.ID, COALESCE(P.名称,'仲間になっていない！') AS なまえ, S.コード名称 AS 職業
+  FROM パーティー AS P
+  RIGHT JOIN (SELECT コード値, コード名称
+          FROM コード
+         WHERE コード種別 ='1') AS S
+    ON P.職業コード = S.コード値
 
 -- ◆解答
+-- 正解
+
+-- ◆67
+SELECT K.イベント番号,K.クリア区分, C.コード値 || ':'|| C.コード名称 AS クリア結果
+  FROM 経験イベント AS K
+   RIGHT JOIN (SELECT コード値, コード名称
+          FROM コード
+         WHERE コード種別 ='4') AS C
+    ON K.クリア結果 = C.コード値
+
+-- ◆解答
+-- 正解
+SELECT E.イベント番号, E.クリア区分,
+       C.コード値 || '：' || C.コード名称 AS クリア結果
+  FROM 経験イベント E
+  FULL JOIN (SELECT コード値, コード名称
+               FROM コード
+              WHERE コード種別 ='4') C
+         ON E.クリア結果 = C.コード値
+/*
+-- FULL JOINが使えない場合、以下で代替
+SELECT E.イベント番号, E.クリア区分,
+       E.クリア結果 || '：' || C.コード名称 AS クリア結果
+  FROM 経験イベント E
+  LEFT JOIN (SELECT コード値, コード名称
+               FROM コード
+              WHERE コード種別 ='4') C
+         ON E.クリア結果 = C.コード値
+UNION
+SELECT E.イベント番号, E.クリア区分,
+       C.コード値 || '：' || C.コード名称 AS クリア結果
+  FROM 経験イベント E
+ RIGHT JOIN (SELECT コード値, コード名称
+               FROM コード
+              WHERE コード種別 ='4') C
+         ON E.クリア結果 = C.コード値
+*/
+
+-- →問題文よく読む
+
+-- ◆68
+SELECT I.イベント番号,I.イベント名称, I.前提イベント番号, Z.イベント名称
+  FROM イベント AS I
+   JOIN (SELECT イベント番号,イベント名称
+          FROM イベント) AS Z
+    ON I.前提イベント番号 = Z.イベント番号
+    WHERE I.前提イベント番号 IS NOT NULL
+
+-- ◆解答
+-- 答えは同じ。別に副問合せ＋JOINにする必要ない
+SELECT E1.イベント番号, E1.イベント名称,
+       E1.前提イベント番号, E2.イベント名称 AS 前提イベント名称
+  FROM イベント E1
+  JOIN イベント E2
+    ON E1.前提イベント番号 = E2.イベント番号
+ WHERE E1.前提イベント番号 IS NOT NULL
